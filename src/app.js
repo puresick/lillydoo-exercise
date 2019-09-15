ccfef28069ba8bb9a8366fd4f8eebff646875535
puresick <2714266+puresick@users.noreply.github.com>
2019-09-15 20:32:25 +0200

    Modify configs and defaults

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import Vue from "vue"
 
-import router from "./router"
 import store from "./store"
 import i18n from "./i18n"
 import App from "./components/App"
 
 new Vue({
-	router,
 	store,
 	i18n,
 	el: "#app",

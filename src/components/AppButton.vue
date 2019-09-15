3032612bec417ac7145fdbeeb9bf0e534204957b
puresick <2714266+puresick@users.noreply.github.com>
2019-09-15 20:31:38 +0200

    Add button component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,36 @@
+<template>
+	<button>
+		{{ label }}
+	</button>
+</template>
+
+<script>
+	export default {
+		name: "AppButton",
+		props: {
+			label: {
+				default: "ButtonLabel",
+				type: String
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import "../stylesheets/config";
+
+	button {
+		color: $color--shade-2;
+		background: $color--accent-0;
+		border: none;
+		font-size: $font__size--2;
+		line-height: 2.8;
+		text-transform: uppercase;
+		height: 45px;
+		width: 100%;
+
+		&:hover {
+			background: $color--accent-1;
+		}
+	}
+</style>

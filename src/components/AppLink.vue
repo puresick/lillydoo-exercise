84e20c8dfaf5962899661247197cec7769cc5fec
puresick <2714266+puresick@users.noreply.github.com>
2019-09-22 12:25:47 +0200

    Add AppLink component

---
+++ src/components/AppLink.vue
@@ -0,0 +1,30 @@
+<template>
+		<a
+			:href="href"
+			:class="{ centered }"
+		>
+			<slot />
+		</a>
+</template>
+
+<script>
+	export default {
+		name: "AppLink",
+		props: {
+			centered: {
+				default: false,
+				type: Boolean
+			},
+			href: {
+				default: "",
+				type: String
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.centered {
+		text-align: center;
+	}
+</style>

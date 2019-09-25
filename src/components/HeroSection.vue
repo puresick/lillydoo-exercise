f5cd11be5398da915e25bdd1058b7404cd2c2b9b
puresick <2714266+puresick@users.noreply.github.com>
2019-09-25 23:19:12 +0200

    Add cta method for scrolling to ProductPicker on user input

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,14 +1,14 @@
 <template>
 	<AppSection>
-		<div>
+		<div id="overlay">
 			<HeroHeadline>
 				{{ content.headline }}
 			</HeroHeadline>
 			<AppButton
 				:label="content.cta"
+				:action="cta"
 			/>
 		</div>
-		<div />
 	</AppSection>
 </template>
 
@@ -29,38 +29,63 @@
 				default: {},
 				type: Object
 			}
+		},
+		methods: {
+			cta() {
+				document.querySelector("#picker").scrollIntoView({
+					behavior: "smooth"
+				})
+			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
+	@import "../stylesheets/config";
 	@import "../stylesheets/mixins";
 
 	section {
 		background: url("../../assets/images/header-trialbox-mobile.jpg");
-		background-size: center;
+		background-size: cover;
 		background-repeat: no-repeat;
+		padding: $gap--constraint__section--hero__base;
+		min-height: $size--min-height--section__hero__base;
+		display: flex;
+		flex-direction: column-reverse;
+		
 
 		@include respond-from("medium") {
 			background: url("../../assets/images/header-trialbox-tablet.jpg");
-			background-size: center;
+			background-size: cover;
 			background-repeat: no-repeat;
 		};
 
 		@include respond-from("large") {
 			background: url("../../assets/images/header-trialbox-desktop.jpg");
-			background-size: center;
+			background-size: cover;
 			background-repeat: no-repeat;
+			min-height: $size--min-height--section__hero__large;
+			flex-direction: row-reverse;
+			height: 100%;
+			padding: $gap--constraint__section--hero__large;
 		};
 
 		div {
-			flex-basis: 50%;
 			display: flex;
 			flex-direction: column;
 			flex-grow: 1;
 			flex-shrink: 1;
 			justify-content: center;
 			align-items: center;
+			margin: 0;
+			padding: $gap--constraint__section--hero--overlay__base;
+			background: rgba(255, 255, 255, 0.7);
+
+			@include respond-from("large") {
+				justify-content: center;
+				padding: $gap--constraint__section--hero--overlay__large;
+				max-width: $size--overlay--width;
+			}
 		}
 	}
 </style>
